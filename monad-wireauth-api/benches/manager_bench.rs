category-labs/monad-bft

authentication protocol for udp

monad-wireauth-api/benches/manager_bench.rs
@@ -1,0 +1,196 @@
+use api::{Config, TestContext, API};
+use divan::Bencher;
+use rand::rngs::OsRng;
+use std::net::SocketAddr;
+use std::time::Duration;
+use monad_wireauth_protocol::common::PublicKey;
+use monad_wireauth_protocol::messages::DataPacketHeader;
+use zerocopy::{AsBytes, FromBytes};
+
+fn main() {
+    divan::main();
+}
+
+fn create_test_manager() -> (API<TestContext>, PublicKey, TestContext) {
+    let mut rng = OsRng;
+    let (public_key, private_key) = monad_wireauth_protocol::crypto::generate_keypair(&mut rng).unwrap();
+    let psk = [0u8; 32];
+    let config = Config {
+        session_timeout: Duration::from_secs(10),
+        session_timeout_jitter: Duration::from_secs(1),
+        keepalive_interval: Duration::from_secs(5),
+        keepalive_jitter: Duration::from_secs(1),
+        rekey_interval: Duration::from_secs(120),
+        rekey_jitter: Duration::from_secs(10),
+        max_session_duration: Duration::from_secs(150),
+        handshake_rate_limit: 50,
+        handshake_rate_reset_interval: Duration::from_secs(1),
+        cookie_refresh_duration: Duration::from_secs(120),
+        low_watermark_sessions: 100,
+        high_watermark_sessions: 500,
+        max_sessions_per_ip: 10,
+        ip_rate_limit_window: Duration::from_secs(60),
+        max_requests_per_ip: 10,
+        ip_history_capacity: 100_000,
+        psk,
+    };
+    let context = TestContext::new();
+    let context_clone = context.clone();
+
+    let manager = API::new(config, private_key, public_key.clone(), context);
+    (manager, public_key, context_clone)
+}
+
+fn establish_session(
+    peer1_manager: &mut API<TestContext>,
+    peer2_manager: &mut API<TestContext>,
+    _peer1_public: &PublicKey,
+    peer2_public: &PublicKey,
+) {
+    let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+    let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+    peer1_manager
+        .connect(
+            peer2_public.clone(),
+            peer2_addr,
+            monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+        )
+        .expect("peer1 failed to init session");
+
+    let init_packet = peer1_manager.next_packet().unwrap().1;
+
+    let mut init_packet_mut = init_packet.to_vec();
+    peer2_manager
+        .dispatch(&mut init_packet_mut, peer1_addr)
+        .expect("peer2 failed to accept handshake");
+
+    let response_packet = peer2_manager.next_packet().unwrap().1;
+
+    let mut response_packet_mut = response_packet.to_vec();
+    peer1_manager
+        .dispatch(&mut response_packet_mut, peer2_addr)
+        .expect("peer1 failed to complete handshake");
+
+    while let Some((_addr, packet)) = peer1_manager.next_packet() {
+        let mut packet_mut = packet.to_vec();
+        peer2_manager.dispatch(&mut packet_mut, peer1_addr).ok();
+    }
+}
+
+#[divan::bench]
+fn bench_session_send_init(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (manager, _local_public, _) = create_test_manager();
+            let (_peer2_manager, peer2_public, _) = create_test_manager();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            (manager, peer2_public, peer2_addr)
+        })
+        .bench_local_values(|(mut manager, peer2_public, peer2_addr)| {
+            manager
+                .connect(peer2_public, peer2_addr, monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS)
+                .expect("failed to init session");
+        });
+}
+
+#[divan::bench]
+fn bench_session_handle_init(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (mut peer1_manager, _peer1_public, _) = create_test_manager();
+            let (peer2_manager, peer2_public, _) = create_test_manager();
+            let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            peer1_manager
+                .connect(peer2_public, peer2_addr, monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS)
+                .expect("failed to init session");
+            let init_packet = peer1_manager.next_packet().unwrap().1;
+
+            (peer2_manager, init_packet, peer1_addr)
+        })
+        .bench_local_values(|(mut peer2_manager, init_packet, peer1_addr)| {
+            let mut init_packet_mut = init_packet.to_vec();
+            peer2_manager
+                .dispatch(&mut init_packet_mut, peer1_addr)
+                .expect("failed to handle init");
+        });
+}
+
+#[divan::bench]
+fn bench_session_handle_response(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (mut mgr1, _peer1_public, _) = create_test_manager();
+            let (mut mgr2, peer2_public, _) = create_test_manager();
+            let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            mgr1.connect(
+                peer2_public.clone(),
+                peer2_addr,
+                monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+            )
+            .expect("init failed");
+            let init_packet = mgr1.next_packet().unwrap().1;
+
+            let mut init_packet_mut = init_packet.to_vec();
+            mgr2.dispatch(&mut init_packet_mut, peer1_addr)
+                .expect("dispatch failed");
+            let response_packet = mgr2.next_packet().unwrap().1;
+
+            (mgr1, response_packet, peer2_addr)
+        })
+        .bench_local_values(|(mut mgr1, response_packet, peer2_addr)| {
+            let mut response_packet_mut = response_packet.to_vec();
+            mgr1.dispatch(&mut response_packet_mut, peer2_addr)
+                .expect("handle response failed");
+        });
+}
+
+#[divan::bench]
+fn bench_session_encrypt(bencher: Bencher) {
+    let (mut mgr1, peer1_public, _) = create_test_manager();
+    let (mut mgr2, peer2_public, _) = create_test_manager();
+
+    establish_session(&mut mgr1, &mut mgr2, &peer1_public, &peer2_public);
+
+    let mut plaintext = vec![0u8; 1024];
+    bencher.bench_local(|| {
+        mgr1.encrypt_by_public_key(&peer2_public, &mut plaintext)
+            .expect("encryption failed");
+    });
+}
+
+#[divan::bench]
+fn bench_session_decrypt(bencher: Bencher) {
+    let (mut mgr1, peer1_public, _) = create_test_manager();
+    let (mut mgr2, peer2_public, _) = create_test_manager();
+
+    establish_session(&mut mgr1, &mut mgr2, &peer1_public, &peer2_public);
+
+    let mut plaintext = vec![0u8; 1024];
+    let header = mgr1
+        .encrypt_by_public_key(&peer2_public, &mut plaintext)
+        .expect("encryption failed");
+
+    let mut packet_data = Vec::with_capacity(header.as_bytes().len() + plaintext.len());
+    packet_data.extend_from_slice(header.as_bytes());
+    packet_data.extend_from_slice(&plaintext);
+
+    let header_ref = DataPacketHeader::ref_from(&packet_data[..DataPacketHeader::SIZE])
+        .expect("failed to get header");
+    let receiver_index = header_ref.receiver_index.get();
+
+    let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+
+    bencher.bench_local(|| {
+        mgr2.reset_replay_filter_for_receiver(receiver_index);
+
+        let mut packet_clone = packet_data.clone();
+        mgr2.dispatch(&mut packet_clone, peer1_addr)
+            .expect("decryption failed");
+    });
+}

monad-wireauth-api/tests/e2e.rs
@@ -1,0 +1,208 @@
+use monad_wireauth_api::{Config, StdContext, API};
+use bytes::Bytes;
+use monoio::net::udp::UdpSocket;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
+use monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS;
+use std::rc::Rc;
+use std::time::Duration;
+use monad_wireauth_protocol::common::PublicKey;
+use zerocopy::AsBytes;
+
+struct PeerNode {
+    manager: API<StdContext>,
+    socket: Rc<UdpSocket>,
+    public_key: PublicKey,
+}
+
+impl PeerNode {
+    fn new(port: u16, seed: u64) -> std::io::Result<Self> {
+        let mut rng = StdRng::seed_from_u64(seed);
+        let (public_key, private_key) =
+            monad_wireauth_protocol::crypto::generate_keypair(&mut rng).unwrap();
+
+        let config = Config {
+            session_timeout: Duration::from_secs(10),
+            session_timeout_jitter: Duration::ZERO,
+            keepalive_interval: Duration::from_secs(3),
+            keepalive_jitter: Duration::ZERO,
+            rekey_interval: Duration::from_secs(60),
+            rekey_jitter: Duration::ZERO,
+            ..Default::default()
+        };
+
+        let context = StdContext::new();
+        let manager = API::new(config, private_key, public_key.clone(), context);
+
+        let addr = format!("127.0.0.1:{}", port);
+        let socket = UdpSocket::bind(addr)?;
+
+        Ok(Self {
+            manager,
+            socket: Rc::new(socket),
+            public_key,
+        })
+    }
+
+    fn connect(&mut self, peer_public: PublicKey, peer_addr: std::net::SocketAddr) {
+        self.manager
+            .connect(peer_public, peer_addr, DEFAULT_RETRY_ATTEMPTS)
+            .unwrap();
+    }
+
+    async fn send_all_packets(&mut self) -> std::io::Result<()> {
+        while let Some((addr, packet)) = self.manager.next_packet() {
+            let packet_vec = packet.to_vec();
+            let (result, _) = self.socket.send_to(packet_vec, addr).await;
+            result?;
+        }
+        Ok(())
+    }
+
+    async fn recv_and_dispatch(&mut self) -> std::io::Result<Option<Bytes>> {
+        let buf = vec![0u8; 65536];
+        let (result, mut buf) = self.socket.recv_from(buf).await;
+        let (len, src) = result?;
+        let result = self.manager.dispatch(&mut buf[..len], src);
+        match result {
+            Ok(data) => Ok(data),
+            Err(_) => Ok(None),
+        }
+    }
+
+    fn encrypt_by_public_key(
+        &mut self,
+        peer_public: &PublicKey,
+        plaintext: &mut [u8],
+    ) -> monad_wireauth_api::Result<monad_wireauth_protocol::messages::DataPacketHeader> {
+        self.manager.encrypt_by_public_key(peer_public, plaintext)
+    }
+}
+
+async fn exchange_handshake(alice: &mut PeerNode, bob: &mut PeerNode) -> std::io::Result<()> {
+    let mut iterations = 0;
+    let max_iterations = 10;
+
+    while iterations < max_iterations {
+        alice.send_all_packets().await?;
+        bob.send_all_packets().await?;
+
+        let alice_socket = alice.socket.clone();
+        let bob_socket = bob.socket.clone();
+
+        let alice_task = async {
+            let buf = vec![0u8; 65536];
+            monoio::select! {
+                recv_result = alice_socket.recv_from(buf) => {
+                    let (result, mut buf) = recv_result;
+                    if let Ok((len, src)) = result {
+                        let _ = alice.manager.dispatch(&mut buf[..len], src);
+                        true
+                    } else {
+                        false
+                    }
+                },
+                _ = monoio::time::sleep(Duration::from_millis(10)) => {
+                    false
+                },
+            }
+        };
+
+        let bob_task = async {
+            let buf = vec![0u8; 65536];
+            monoio::select! {
+                recv_result = bob_socket.recv_from(buf) => {
+                    let (result, mut buf) = recv_result;
+                    if let Ok((len, src)) = result {
+                        let _ = bob.manager.dispatch(&mut buf[..len], src);
+                        true
+                    } else {
+                        false
+                    }
+                },
+                _ = monoio::time::sleep(Duration::from_millis(10)) => {
+                    false
+                },
+            }
+        };
+
+        let (alice_received, bob_received) = monoio::join!(alice_task, bob_task);
+
+        if !alice_received && !bob_received {
+            break;
+        }
+
+        iterations += 1;
+    }
+
+    Ok(())
+}
+
+#[monoio::test(timer_enabled = true)]
+async fn test_e2e_handshake_and_data() {
+    let mut alice = PeerNode::new(28001, 1).unwrap();
+    let mut bob = PeerNode::new(28002, 2).unwrap();
+
+    let bob_addr = bob.socket.local_addr().unwrap();
+    alice.connect(bob.public_key.clone(), bob_addr);
+
+    exchange_handshake(&mut alice, &mut bob).await.unwrap();
+
+    let mut plaintext = b"hello from alice".to_vec();
+    let header = alice
+        .encrypt_by_public_key(&bob.public_key, &mut plaintext)
+        .expect("alice encrypt failed");
+
+    let mut packet = Vec::new();
+    packet.extend_from_slice(header.as_bytes());
+    packet.extend_from_slice(&plaintext);
+
+    let (result, _) = alice.socket.send_to(packet, bob_addr).await;
+    result.unwrap();
+
+    let received = bob.recv_and_dispatch().await.unwrap();
+    assert_eq!(received, Some(Bytes::from(&b"hello from alice"[..])));
+}
+
+#[monoio::test(timer_enabled = true)]
+async fn test_e2e_bidirectional() {
+    let mut alice = PeerNode::new(28003, 3).unwrap();
+    let mut bob = PeerNode::new(28004, 4).unwrap();
+
+    let bob_addr = bob.socket.local_addr().unwrap();
+    let alice_addr = alice.socket.local_addr().unwrap();
+
+    alice.connect(bob.public_key.clone(), bob_addr);
+
+    exchange_handshake(&mut alice, &mut bob).await.unwrap();
+
+    let mut msg_alice = b"message from alice".to_vec();
+    let header_alice = alice
+        .encrypt_by_public_key(&bob.public_key, &mut msg_alice)
+        .unwrap();
+
+    let mut packet_alice = Vec::new();
+    packet_alice.extend_from_slice(header_alice.as_bytes());
+    packet_alice.extend_from_slice(&msg_alice);
+
+    let (result, _) = alice.socket.send_to(packet_alice, bob_addr).await;
+    result.unwrap();
+
+    let received_bob = bob.recv_and_dispatch().await.unwrap();
+    assert_eq!(received_bob, Some(Bytes::from(&b"message from alice"[..])));
+
+    let mut msg_bob = b"message from bob".to_vec();
+    let header_bob = bob
+        .encrypt_by_public_key(&alice.public_key, &mut msg_bob)
+        .unwrap();
+
+    let mut packet_bob = Vec::new();
+    packet_bob.extend_from_slice(header_bob.as_bytes());
+    packet_bob.extend_from_slice(&msg_bob);
+
+    let (result, _) = bob.socket.send_to(packet_bob, alice_addr).await;
+    result.unwrap();
+
+    let received_alice = alice.recv_and_dispatch().await.unwrap();
+    assert_eq!(received_alice, Some(Bytes::from(&b"message from bob"[..])));
+}

Bullayer/monad-bft-xyz

monad-wireauth-api/benches/manager_bench.rs
@@ -1,0 +1,204 @@
+use std::{net::SocketAddr, time::Duration};
+
+use monad_wireauth_api::{Config, TestContext, API};
+use divan::Bencher;
+use monad_wireauth_protocol::{common::PublicKey, messages::DataPacketHeader};
+use secp256k1::rand::rng;
+use zerocopy::{FromBytes, IntoBytes};
+
+fn main() {
+    divan::main();
+}
+
+fn create_test_manager() -> (API<TestContext>, PublicKey, TestContext) {
+    let mut rng = rng();
+    let (public_key, private_key) =
+        monad_wireauth_protocol::crypto::generate_keypair(&mut rng).unwrap();
+    let psk = [0u8; 32];
+    let config = Config {
+        session_timeout: Duration::from_secs(10),
+        session_timeout_jitter: Duration::from_secs(1),
+        keepalive_interval: Duration::from_secs(5),
+        keepalive_jitter: Duration::from_secs(1),
+        rekey_interval: Duration::from_secs(120),
+        rekey_jitter: Duration::from_secs(10),
+        max_session_duration: Duration::from_secs(150),
+        handshake_rate_limit: 50,
+        handshake_rate_reset_interval: Duration::from_secs(1),
+        cookie_refresh_duration: Duration::from_secs(120),
+        low_watermark_sessions: 100,
+        high_watermark_sessions: 500,
+        max_sessions_per_ip: 10,
+        ip_rate_limit_window: Duration::from_secs(60),
+        max_requests_per_ip: 10,
+        ip_history_capacity: 100_000,
+        psk,
+    };
+    let context = TestContext::new();
+    let context_clone = context.clone();
+
+    let manager = API::new(config, private_key, public_key.clone(), context);
+    (manager, public_key, context_clone)
+}
+
+fn establish_session(
+    peer1_manager: &mut API<TestContext>,
+    peer2_manager: &mut API<TestContext>,
+    _peer1_public: &PublicKey,
+    peer2_public: &PublicKey,
+) {
+    let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+    let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+    peer1_manager
+        .connect(
+            peer2_public.clone(),
+            peer2_addr,
+            monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+        )
+        .expect("peer1 failed to init session");
+
+    let init_packet = peer1_manager.next_packet().unwrap().1;
+
+    let mut init_packet_mut = init_packet.to_vec();
+    peer2_manager
+        .dispatch(&mut init_packet_mut, peer1_addr)
+        .expect("peer2 failed to accept handshake");
+
+    let response_packet = peer2_manager.next_packet().unwrap().1;
+
+    let mut response_packet_mut = response_packet.to_vec();
+    peer1_manager
+        .dispatch(&mut response_packet_mut, peer2_addr)
+        .expect("peer1 failed to complete handshake");
+
+    while let Some((_addr, packet)) = peer1_manager.next_packet() {
+        let mut packet_mut = packet.to_vec();
+        peer2_manager.dispatch(&mut packet_mut, peer1_addr).ok();
+    }
+}
+
+#[divan::bench]
+fn bench_session_send_init(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (manager, _local_public, _) = create_test_manager();
+            let (_peer2_manager, peer2_public, _) = create_test_manager();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            (manager, peer2_public, peer2_addr)
+        })
+        .bench_local_values(|(mut manager, peer2_public, peer2_addr)| {
+            manager
+                .connect(
+                    peer2_public,
+                    peer2_addr,
+                    monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+                )
+                .expect("failed to init session");
+        });
+}
+
+#[divan::bench]
+fn bench_session_handle_init(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (mut peer1_manager, _peer1_public, _) = create_test_manager();
+            let (peer2_manager, peer2_public, _) = create_test_manager();
+            let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            peer1_manager
+                .connect(
+                    peer2_public,
+                    peer2_addr,
+                    monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+                )
+                .expect("failed to init session");
+            let init_packet = peer1_manager.next_packet().unwrap().1;
+
+            (peer2_manager, init_packet, peer1_addr)
+        })
+        .bench_local_values(|(mut peer2_manager, init_packet, peer1_addr)| {
+            let mut init_packet_mut = init_packet.to_vec();
+            peer2_manager
+                .dispatch(&mut init_packet_mut, peer1_addr)
+                .expect("failed to handle init");
+        });
+}
+
+#[divan::bench]
+fn bench_session_handle_response(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            let (mut mgr1, _peer1_public, _) = create_test_manager();
+            let (mut mgr2, peer2_public, _) = create_test_manager();
+            let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+            let peer2_addr: SocketAddr = "127.0.0.1:51821".parse().unwrap();
+
+            mgr1.connect(
+                peer2_public,
+                peer2_addr,
+                monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS,
+            )
+            .expect("init failed");
+            let init_packet = mgr1.next_packet().unwrap().1;
+
+            let mut init_packet_mut = init_packet.to_vec();
+            mgr2.dispatch(&mut init_packet_mut, peer1_addr)
+                .expect("dispatch failed");
+            let response_packet = mgr2.next_packet().unwrap().1;
+
+            (mgr1, response_packet, peer2_addr)
+        })
+        .bench_local_values(|(mut mgr1, response_packet, peer2_addr)| {
+            let mut response_packet_mut = response_packet.to_vec();
+            mgr1.dispatch(&mut response_packet_mut, peer2_addr)
+                .expect("handle response failed");
+        });
+}
+
+#[divan::bench]
+fn bench_session_encrypt(bencher: Bencher) {
+    let (mut mgr1, peer1_public, _) = create_test_manager();
+    let (mut mgr2, peer2_public, _) = create_test_manager();
+
+    establish_session(&mut mgr1, &mut mgr2, &peer1_public, &peer2_public);
+
+    let mut plaintext = vec![0u8; 1024];
+    bencher.bench_local(|| {
+        mgr1.encrypt_by_public_key(&peer2_public, &mut plaintext)
+            .expect("encryption failed");
+    });
+}
+
+#[divan::bench]
+fn bench_session_decrypt(bencher: Bencher) {
+    let (mut mgr1, peer1_public, _) = create_test_manager();
+    let (mut mgr2, peer2_public, _) = create_test_manager();
+
+    establish_session(&mut mgr1, &mut mgr2, &peer1_public, &peer2_public);
+
+    let mut plaintext = vec![0u8; 1024];
+    let header = mgr1
+        .encrypt_by_public_key(&peer2_public, &mut plaintext)
+        .expect("encryption failed");
+
+    let mut packet_data = Vec::with_capacity(header.as_bytes().len() + plaintext.len());
+    packet_data.extend_from_slice(header.as_bytes());
+    packet_data.extend_from_slice(&plaintext);
+
+    let header_ref = DataPacketHeader::ref_from_bytes(&packet_data[..DataPacketHeader::SIZE])
+        .expect("failed to get header");
+    let receiver_index = header_ref.receiver_index.get();
+
+    let peer1_addr: SocketAddr = "127.0.0.1:51820".parse().unwrap();
+
+    bencher.bench_local(|| {
+        mgr2.reset_replay_filter_for_receiver(receiver_index);
+
+        let mut packet_clone = packet_data.clone();
+        mgr2.dispatch(&mut packet_clone, peer1_addr)
+            .expect("decryption failed");
+    });
+}

monad-wireauth-api/tests/e2e.rs
@@ -1,0 +1,207 @@
+use std::{rc::Rc, time::Duration};
+
+use bytes::Bytes;
+use monad_wireauth_api::{Config, StdContext, API};
+use monad_wireauth_protocol::common::PublicKey;
+use monad_wireauth_session::DEFAULT_RETRY_ATTEMPTS;
+use monoio::net::udp::UdpSocket;
+use secp256k1::rand::{rngs::StdRng, SeedableRng};
+use zerocopy::IntoBytes;
+
+struct PeerNode {
+    manager: API<StdContext>,
+    socket: Rc<UdpSocket>,
+    public_key: PublicKey,
+}
+
+impl PeerNode {
+    fn new(port: u16, seed: u64) -> std::io::Result<Self> {
+        let mut rng = StdRng::seed_from_u64(seed);
+        let (public_key, private_key) =
+            monad_wireauth_protocol::crypto::generate_keypair(&mut rng).unwrap();
+
+        let config = Config {
+            session_timeout: Duration::from_secs(10),
+            session_timeout_jitter: Duration::ZERO,
+            keepalive_interval: Duration::from_secs(3),
+            keepalive_jitter: Duration::ZERO,
+            rekey_interval: Duration::from_secs(60),
+            rekey_jitter: Duration::ZERO,
+            ..Default::default()
+        };
+
+        let context = StdContext::new();
+        let manager = API::new(config, private_key, public_key.clone(), context);
+
+        let addr = format!("127.0.0.1:{}", port);
+        let socket = UdpSocket::bind(addr)?;
+
+        Ok(Self {
+            manager,
+            socket: Rc::new(socket),
+            public_key,
+        })
+    }
+
+    fn connect(&mut self, peer_public: PublicKey, peer_addr: std::net::SocketAddr) {
+        self.manager
+            .connect(peer_public, peer_addr, DEFAULT_RETRY_ATTEMPTS)
+            .unwrap();
+    }
+
+    async fn send_all_packets(&mut self) -> std::io::Result<()> {
+        while let Some((addr, packet)) = self.manager.next_packet() {
+            let packet_vec = packet.to_vec();
+            let (result, _) = self.socket.send_to(packet_vec, addr).await;
+            result?;
+        }
+        Ok(())
+    }
+
+    async fn recv_and_dispatch(&mut self) -> std::io::Result<Option<Bytes>> {
+        let buf = vec![0u8; 65536];
+        let (result, mut buf) = self.socket.recv_from(buf).await;
+        let (len, src) = result?;
+        let result = self.manager.dispatch(&mut buf[..len], src);
+        match result {
+            Ok(data) => Ok(data),
+            Err(_) => Ok(None),
+        }
+    }
+
+    fn encrypt_by_public_key(
+        &mut self,
+        peer_public: &PublicKey,
+        plaintext: &mut [u8],
+    ) -> monad_wireauth_api::Result<monad_wireauth_protocol::messages::DataPacketHeader> {
+        self.manager.encrypt_by_public_key(peer_public, plaintext)
+    }
+}
+
+async fn exchange_handshake(alice: &mut PeerNode, bob: &mut PeerNode) -> std::io::Result<()> {
+    let mut iterations = 0;
+    let max_iterations = 10;
+
+    while iterations < max_iterations {
+        alice.send_all_packets().await?;
+        bob.send_all_packets().await?;
+
+        let alice_socket = alice.socket.clone();
+        let bob_socket = bob.socket.clone();
+
+        let alice_task = async {
+            let buf = vec![0u8; 65536];
+            monoio::select! {
+                recv_result = alice_socket.recv_from(buf) => {
+                    let (result, mut buf) = recv_result;
+                    if let Ok((len, src)) = result {
+                        let _ = alice.manager.dispatch(&mut buf[..len], src);
+                        true
+                    } else {
+                        false
+                    }
+                },
+                _ = monoio::time::sleep(Duration::from_millis(10)) => {
+                    false
+                },
+            }
+        };
+
+        let bob_task = async {
+            let buf = vec![0u8; 65536];
+            monoio::select! {
+                recv_result = bob_socket.recv_from(buf) => {
+                    let (result, mut buf) = recv_result;
+                    if let Ok((len, src)) = result {
+                        let _ = bob.manager.dispatch(&mut buf[..len], src);
+                        true
+                    } else {
+                        false
+                    }
+                },
+                _ = monoio::time::sleep(Duration::from_millis(10)) => {
+                    false
+                },
+            }
+        };
+
+        let (alice_received, bob_received) = monoio::join!(alice_task, bob_task);
+
+        if !alice_received && !bob_received {
+            break;
+        }
+
+        iterations += 1;
+    }
+
+    Ok(())
+}
+
+#[monoio::test(timer_enabled = true)]
+async fn test_e2e_handshake_and_data() {
+    let mut alice = PeerNode::new(28001, 1).unwrap();
+    let mut bob = PeerNode::new(28002, 2).unwrap();
+
+    let bob_addr = bob.socket.local_addr().unwrap();
+    alice.connect(bob.public_key.clone(), bob_addr);
+
+    exchange_handshake(&mut alice, &mut bob).await.unwrap();
+
+    let mut plaintext = b"hello from alice".to_vec();
+    let header = alice
+        .encrypt_by_public_key(&bob.public_key, &mut plaintext)
+        .expect("alice encrypt failed");
+
+    let mut packet = Vec::new();
+    packet.extend_from_slice(header.as_bytes());
+    packet.extend_from_slice(&plaintext);
+
+    let (result, _) = alice.socket.send_to(packet, bob_addr).await;
+    result.unwrap();
+
+    let received = bob.recv_and_dispatch().await.unwrap();
+    assert_eq!(received, Some(Bytes::from(&b"hello from alice"[..])));
+}
+
+#[monoio::test(timer_enabled = true)]
+async fn test_e2e_bidirectional() {
+    let mut alice = PeerNode::new(28003, 3).unwrap();
+    let mut bob = PeerNode::new(28004, 4).unwrap();
+
+    let bob_addr = bob.socket.local_addr().unwrap();
+    let alice_addr = alice.socket.local_addr().unwrap();
+
+    alice.connect(bob.public_key.clone(), bob_addr);
+
+    exchange_handshake(&mut alice, &mut bob).await.unwrap();
+
+    let mut msg_alice = b"message from alice".to_vec();
+    let header_alice = alice
+        .encrypt_by_public_key(&bob.public_key, &mut msg_alice)
+        .unwrap();
+
+    let mut packet_alice = Vec::new();
+    packet_alice.extend_from_slice(header_alice.as_bytes());
+    packet_alice.extend_from_slice(&msg_alice);
+
+    let (result, _) = alice.socket.send_to(packet_alice, bob_addr).await;
+    result.unwrap();
+
+    let received_bob = bob.recv_and_dispatch().await.unwrap();
+    assert_eq!(received_bob, Some(Bytes::from(&b"message from alice"[..])));
+
+    let mut msg_bob = b"message from bob".to_vec();
+    let header_bob = bob
+        .encrypt_by_public_key(&alice.public_key, &mut msg_bob)
+        .unwrap();
+
+    let mut packet_bob = Vec::new();
+    packet_bob.extend_from_slice(header_bob.as_bytes());
+    packet_bob.extend_from_slice(&msg_bob);
+
+    let (result, _) = bob.socket.send_to(packet_bob, alice_addr).await;
+    result.unwrap();
+
+    let received_alice = alice.recv_and_dispatch().await.unwrap();
+    assert_eq!(received_alice, Some(Bytes::from(&b"message from bob"[..])));
+}
a3b70036a07ff444268705e4cc062a28aa121e3f
kobe <kobe@192.168.2.103>
2026-02-04 10:36:49 +0800

    nothing

--- monad-raptorcast/src/lib.rs
+++ monad-raptorcast/src/lib.rs
@@ -689,7 +689,6 @@ where
 
     fn exec(&mut self, commands: Vec<Self::Command>) {
         let self_id = NodeId::new(self.signing_key.pubkey());
-        warn!("RaptorCast self_id is {}", self_id);
 
         for command in commands {
             match command {

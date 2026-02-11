be4997228f3de35f7ef1d542318de09dd112a5d8
harrybulls <harry@bullayer.com>
2026-02-11 14:44:21 +0800

    fix some

--- hyperliquid-test/src/main.rs
+++ hyperliquid-test/src/main.rs
@@ -1,5 +1,4 @@
 //! 简化的 Proposal 提案演示
-
 use std::fmt::Debug;
 use tracing::info;
 
@@ -80,13 +79,11 @@ impl<ST, T> Proposal<ST, T> {
 fn main() {
     tracing_subscriber::fmt::init();
 
-    info!("start create a new proposal");
+    info!("start create new proposal");
 
-    // 验证者
     let validator = VALIDATOR;
     info!(?validator, "Validator:");
 
-    // 提案 Round 1
     let proposal = create_proposal();
 
     assert!(proposal.proposer == validator);
@@ -98,27 +95,39 @@ fn main() {
 // 构建proposal
 fn create_proposal() -> Proposal<String, String> {
     // 1. 创建 QuorumCertificate
+    let qc_round = 101;
+    let qc_epoch = 1;
+    let qc_block_hash = "0x456ef06527d887cd11fc11f1990bf8cb0d8adaa15c3ddca252258c1b622d0f2a";
+    let qc_sig1 = "0x1234";
+    let qc_sig2 = "0x5678";
+    let signatures = vec![qc_sig1.to_string(), qc_sig2.to_string()];
     let qc = QuorumCertificate::<String>::new(
-        101,
-        1,
-        "0x456ef06527d887cd11fc11f1990bf8cb0d8adaa15c3ddca252258c1b622d0f2a".to_string(),
-        vec!["0x0102030405".to_string()],
+        qc_round,
+        qc_epoch,
+        qc_block_hash.to_string(),
+        signatures,
     );
 
     // 2. 创建 BlockData (用于 Proposal)
+    let block_height = 102;
+    let block_hash = "0x3840323385af75b8ba820eb690152717c0e565a2fb30f439ce33b6080ec56e83";
+    let parent_hash = "0x456ef06527d887cd11fc11f1990bf8cb0d8adaa15c3ddca252258c1b622d0f2a";
+    let txs = vec![];
     let proposal_block_data = BlockData::<String>::new(
-        102,
-        "0x3840323385af75b8ba820eb690152717c0e565a2fb30f439ce33b6080ec56e83".to_string(),
-        "0x456ef06527d887cd11fc11f1990bf8cb0d8adaa15c3ddca252258c1b622d0f2a".to_string(),
-        vec![],
+        block_height,
+        block_hash.to_string(),
+        parent_hash.to_string(),
+        txs,
     );
 
-    // 标准 Secp256k1 签名: 65 字节 (r: 32字节, s: 32字节, v: 1字节)
+    // 标准 Secp256k1
+    let round = 102;
+    let epoch = 1;
     let secp256k1_sig = "0xa5b9c8d3e4f7a1b2c6d8e0f4a2b1c3d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b3c4d5e6f7a8b9";
     Proposal::<String, String>::new(
         VALIDATOR.to_string(),
-        102,
-        1,
+        round,
+        epoch,
         qc,
         proposal_block_data,
         secp256k1_sig.to_string(),
